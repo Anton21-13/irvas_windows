0e12e330f95fa57c6f397fa878b751ba146d8e5f
Антон Мещеряков <anton23.pochta@gmail.com>
2019-03-30 00:52:41 +0500

    made timer

--- public/bundle.js
+++ public/bundle.js
@@ -385,48 +385,67 @@ module.exports = tabs;
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-function timer() {// let deadline = '2019-03-23';
-  // function getTimeRamaining(endtime) {
-  //   let t = Date.parse(endtime) - Date.parse(new Date()),
-  //   seconds = Math.floor((t/1000) % 60),
-  //   minutes = Math.floor((t/1000/60) % 60),
-  //   hours = Math.floor((t/1000/60/60) % 24);
-  //   if (hours < 10) {
-  //     hours = `0 ${hours}`;
-  //   }
-  //   if (minutes < 10) {
-  //     minutes = `0 ${minutes}`;
-  //   }
-  //   if (seconds < 10) {
-  //     seconds = `0 ${seconds}`;
-  //   }
-  //   return {
-  //     'total' : t,
-  //     'hours' : hours,
-  //     'minutes' : minutes,
-  //     'seconds' : seconds
-  //   };
-  // }
-  // function setClock(id, endtime) {
-  //   let timer =document.getElementById(id),
-  //     hours = timer.querySelector('.hours'),
-  //     minutes = timer.querySelector('.minutes'),
-  //     seconds = timer.querySelector('.seconds'),
-  //     timeInterval = setInterval(updateClock, 1000);
-  //   function updateClock() {
-  //     let t = getTimeRamaining(endtime);
-  //       hours.textContent = t.hours;
-  //       minutes.textContent = t.minutes;
-  //       seconds.textContent = t.seconds;
-  //     if (t.total <= 0) {
-  //       clearInterval(timeInterval);
-  //       hours.textContent = "00";
-  //       seconds.textContent = "00";
-  //       minutes.textContent = "00";
-  //     }
-  //   }  
-  // }
-  // setClock('timer', deadline);
+function timer() {
+  var deadline = '2019-04-07';
+
+  function getTimeRamaining(endtime) {
+    var t = Date.parse(endtime) - Date.parse(new Date()),
+        seconds = Math.floor(t / 1000 % 60),
+        minutes = Math.floor(t / 1000 / 60 % 60),
+        hours = Math.floor(t / 1000 / 60 / 60 % 24);
+    days = Math.floor(t / 1000 / 60 / 60 / 24 % 24);
+
+    if (days < 10) {
+      days = "0 ".concat(days);
+    }
+
+    if (hours < 10) {
+      hours = "0 ".concat(hours);
+    }
+
+    if (minutes < 10) {
+      minutes = "0 ".concat(minutes);
+    }
+
+    if (seconds < 10) {
+      seconds = "0 ".concat(seconds);
+    }
+
+    return {
+      'total': t,
+      'days': days,
+      'hours': hours,
+      'minutes': minutes,
+      'seconds': seconds
+    };
+  }
+
+  function setClock(id, endtime) {
+    var timer = document.getElementById(id),
+        days = timer.querySelector('#days'),
+        hours = timer.querySelector('#hours'),
+        minutes = timer.querySelector('#minutes'),
+        seconds = timer.querySelector('#seconds'),
+        timeInterval = setInterval(updateClock, 1000);
+
+    function updateClock() {
+      var t = getTimeRamaining(endtime);
+      days.textContent = t.days;
+      hours.textContent = t.hours;
+      minutes.textContent = t.minutes;
+      seconds.textContent = t.seconds;
+
+      if (t.total <= 0) {
+        clearInterval(timeInterval);
+        days.textContent = "00";
+        hours.textContent = "00";
+        seconds.textContent = "00";
+        minutes.textContent = "00";
+      }
+    }
+  }
+
+  setClock('timer', deadline);
 }
 
 module.exports = timer;
--- src/parts/timer.js
+++ src/parts/timer.js
@@ -1,56 +1,65 @@
 function timer() {
 
-  // let deadline = '2019-03-23';
+  let deadline = '2019-04-07';
 
-  // function getTimeRamaining(endtime) {
-  //   let t = Date.parse(endtime) - Date.parse(new Date()),
-  //   seconds = Math.floor((t/1000) % 60),
-  //   minutes = Math.floor((t/1000/60) % 60),
-  //   hours = Math.floor((t/1000/60/60) % 24);
+  function getTimeRamaining(endtime) {
+    let t = Date.parse(endtime) - Date.parse(new Date()),
+    seconds = Math.floor((t/1000) % 60),
+    minutes = Math.floor((t/1000/60) % 60),
+    hours = Math.floor((t/1000/60/60) % 24);
+    days = Math.floor((t/1000/60/60/24) % 24);
 
-  //   if (hours < 10) {
-  //     hours = `0 ${hours}`;
-  //   }
+    if (days < 10) {
+      days = `0 ${days}`;
+    }
 
-  //   if (minutes < 10) {
-  //     minutes = `0 ${minutes}`;
-  //   }
+    if (hours < 10) {
+      hours = `0 ${hours}`;
+    }
 
-  //   if (seconds < 10) {
-  //     seconds = `0 ${seconds}`;
-  //   }
+    if (minutes < 10) {
+      minutes = `0 ${minutes}`;
+    }
 
-  //   return {
-  //     'total' : t,
-  //     'hours' : hours,
-  //     'minutes' : minutes,
-  //     'seconds' : seconds
-  //   };
-  // }
+    if (seconds < 10) {
+      seconds = `0 ${seconds}`;
+    }
 
-  // function setClock(id, endtime) {
-  //   let timer =document.getElementById(id),
-  //     hours = timer.querySelector('.hours'),
-  //     minutes = timer.querySelector('.minutes'),
-  //     seconds = timer.querySelector('.seconds'),
-  //     timeInterval = setInterval(updateClock, 1000);
+    return {
+      'total' : t,
+      'days' : days,
+      'hours' : hours,
+      'minutes' : minutes,
+      'seconds' : seconds
+    };
+  }
 
-  //   function updateClock() {
-  //     let t = getTimeRamaining(endtime);
-  //       hours.textContent = t.hours;
-  //       minutes.textContent = t.minutes;
-  //       seconds.textContent = t.seconds;
+  function setClock(id, endtime) {
+    let timer = document.getElementById(id),
+      days = timer.querySelector('#days'),
+      hours = timer.querySelector('#hours'),
+      minutes = timer.querySelector('#minutes'),
+      seconds = timer.querySelector('#seconds'),
+      timeInterval = setInterval(updateClock, 1000);
 
-  //     if (t.total <= 0) {
-  //       clearInterval(timeInterval);
-  //       hours.textContent = "00";
-  //       seconds.textContent = "00";
-  //       minutes.textContent = "00";
-  //     }
-  //   }  
-  // }
+    function updateClock() {
+      let t = getTimeRamaining(endtime);
+        days.textContent = t.days;
+        hours.textContent = t.hours;
+        minutes.textContent = t.minutes;
+        seconds.textContent = t.seconds;
+
+      if (t.total <= 0) {
+        clearInterval(timeInterval);
+        days.textContent = "00";
+        hours.textContent = "00";
+        seconds.textContent = "00";
+        minutes.textContent = "00";
+      }
+    }  
+  }
   
-  // setClock('timer', deadline);
+  setClock('timer', deadline);
 
 }
 
