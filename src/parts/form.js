d02ee367b6301822ebc5d1f2e8930f1d00181b17
Антон Мещеряков <anton23.pochta@gmail.com>
2019-03-31 21:12:07 +0500

    Выполнил формы

--- src/parts/form.js
+++ src/parts/form.js
@@ -1,87 +1,79 @@
 function form() {
 
+  let message = {
+    loading: 'Загрузка...',
+    success: 'Спасибо! Скоро мы с вами свяжимся!',
+    failure: 'Что-то пошло не так...'
+  };
 
-//   let message = {
-//     loading: 'Загрузка...',
-//     success: 'Спасибо! Скоро мы с вами свяжимся!',
-//     failure: 'Что-то пошло не так...'
-//   };
+  let form = document.querySelectorAll('.form'),
+    input = document.getElementsByTagName('.input'),
+    statusMessage = document.createElement('div'),
+    inputPhone = document.getElementsByName('user_phone');
 
-//   let form = document.querySelector('.main-form'),
-//     contactForm = document.querySelector('#form'),
-//     input = document.querySelectorAll('input'),
-//     statusMessage = document.createElement('div'),
-//     inputPhone = document.querySelectorAll('input[type="tel"]');
+  form.forEach(function submissionForm(element) {
+    element.addEventListener('submit', (a) => {
+      a.preventDefault();
+      element.appendChild(statusMessage);
+      let formData = new FormData(element);
 
-//   statusMessage.classList.add('status');
+      let obj = {};
 
-//   input[2].setAttribute('name', 'email');
-//   input[3].setAttribute('name', 'phone');
+      formData.forEach((value, key) => {
+        obj[key] = value;
+      });
 
-//   function submissionForm(element) {
-//     element.addEventListener('submit', (a) => {
-//       a.preventDefault();
-//       element.appendChild(statusMessage);
-//       let formData = new FormData(element);
-
-//       // let obj = {};
-//       // formData.forEach((value, key) => {
-//       //   obj[key] = value;
-//       // });
-
-//       //let json = JSON.stringify(obj);      
+      let json = JSON.stringify(obj);
       
 
-//       function postData(data) {
-//         return new Promise((resolve, reject) => {
-//           let request = new XMLHttpRequest();
-//           request.open('POST', 'server.php');
-//           request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+      function postData() {
+        return new Promise((resolve, reject) => {
+          let request = new XMLHttpRequest();
+          request.open('POST', 'server.php');
+          request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
 
-//           request.onreadystatechange = () => {
-//             if (request.readyState < 4) {
-//               resolve();
-//             } else if (request.readyState === 4 && request.status == 200) {
-//               resolve();
-//             } else {
-//               reject();
-//             }
-//           };
-//           request.send(/*json*/data);
-//         });
-//       }
+          request.onreadystatechange = () => {
+            if (request.readyState < 4) {
+              resolve();
+            } else if (request.readyState === 4 && request.status == 200) {
+              resolve();
+            } else {
+              reject();
+            }
+          };
+          request.send(json);
+        });
+      }
 
-//       function clearInput() {
-//         for (let i = 0; i < input.length; i++) {
-//           input[i].value = '';
-//         }
-//         statusMessage.innerHTML = '';
-//       }
+      function clearInput() {
+        for (let i = 0; i < input.length; i++) {
+          input[i].value = '';
+        }
+        statusMessage.innerHTML = '';
+      }
 
-//       postData(formData)
-//       .then(() => statusMessage.innerHTML = message.loading)
-//       .then(() => statusMessage.innerHTML = message.success)
-//       .catch(() => statusMessage.innerHTML = message.failure)
-//       .then(() => setTimeout(clearInput, 4000));
-//     });
-//   }
+      postData(formData)
+      .then(() => statusMessage.innerHTML = message.loading)
+      .then(() => statusMessage.innerHTML = message.success)
+      .catch(() => statusMessage.innerHTML = message.failure)
+      .then(() => setTimeout(clearInput, 4000));
+    });
+  });
 
-//   submissionForm(form);
-//   submissionForm(contactForm);
   
-//   inputPhone.forEach((elem) => {
-//     elem.addEventListener('focus', () => {
-//       if(!/^\+\d*$/.test(elem.value)) {
-//         elem.value = '+';
-//       }
-//     });
+  inputPhone.forEach((elem) => {
+    elem.addEventListener('focus', () => {
+      if(!/^\+\d*$/.test(elem.value)) {
+        elem.value = '+';
+      }
+    });
 
-//     elem.addEventListener('keydown', (e) => {
-//       if(!/\d/.test(e.key)) {
-//         e.preventDefault();
-//       }
-//     });
-//   });
+    elem.addEventListener('keydown', (e) => {
+      if(!/\d/.test(e.key)) {
+        e.preventDefault();
+      }
+    });
+  });
 }
 
 module.exports = form;
